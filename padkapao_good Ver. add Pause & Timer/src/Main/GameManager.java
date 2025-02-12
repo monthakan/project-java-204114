e8cb85dedc656525971979241bd4b70539981a6c
Dawn <132326166+empdoctor@users.noreply.github.com>
2025-02-12 17:29:07 +0700

    Add files via upload

---
+++ padkapao_good Ver. add Pause & Timer/src/Main/GameManager.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class GameManager {
+
+	public ActionHandler aHandler = new ActionHandler(this);
+    public UI ui = new UI(this);
+    public SceneChanger sChanger = new SceneChanger(this);
+    
+    
+    public static void main(String[] args) { new GameManager(); }
+    public GameManager() { }
+}
---
+++ padkapao_good Ver. add Pause & Timer/src/Main/SceneChanger.java
@@ -0,0 +1,69 @@
+package Main;
+
+public class SceneChanger {
+
+	GameManager gm;
+	boolean start=false;
+	int bgNow;
+	
+	public SceneChanger(GameManager gm)  {this.gm = gm;}
+	
+	public int getbgNow() {
+		
+		return bgNow;
+	}
+	
+	// show dialouge screen
+	public void showDialogueScreen() {
+		
+		bgNow = 1;
+		
+		gm.ui.bgPanel[0].setVisible(false);
+		gm.ui.bgPanel[1].setVisible(true);
+		gm.ui.bgPanel[2].setVisible(false);
+		gm.ui.bgPanel[3].setVisible(false);
+		gm.ui.bgPanel[4].setVisible(false);
+	}
+	// show select ingredient screen
+	public void showSelectIngScreen() {
+
+		bgNow = 2;
+		
+		gm.ui.bgPanel[0].setVisible(false);
+		gm.ui.bgPanel[1].setVisible(false);
+		gm.ui.bgPanel[2].setVisible(true);
+		gm.ui.bgPanel[3].setVisible(false);
+		gm.ui.bgPanel[4].setVisible(false);
+	}
+	// show stove screen
+	public void showStoveScreen() {
+
+		bgNow = 3;
+		
+		gm.ui.bgPanel[0].setVisible(false);
+		gm.ui.bgPanel[1].setVisible(false);
+		gm.ui.bgPanel[2].setVisible(false);
+		gm.ui.bgPanel[3].setVisible(true);
+		gm.ui.bgPanel[4].setVisible(false);
+	}
+	// show package screen
+	public void showPackageScreen() {
+		
+		bgNow = 4;
+		
+		gm.ui.bgPanel[0].setVisible(false);
+		gm.ui.bgPanel[1].setVisible(false);
+		gm.ui.bgPanel[2].setVisible(false);
+		gm.ui.bgPanel[3].setVisible(false);
+		gm.ui.bgPanel[4].setVisible(true);
+	}
+	
+	public void showGameOver() {
+		
+		gm.ui.bgPanel[0].setVisible(false);
+		gm.ui.bgPanel[1].setVisible(false);
+		gm.ui.bgPanel[2].setVisible(false);
+		gm.ui.bgPanel[3].setVisible(false);
+		gm.ui.bgPanel[4].setVisible(false);
+	}
+}
